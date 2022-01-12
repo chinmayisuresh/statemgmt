dfdf03ee3490c3304dfa768645496132e36f74d5
chinmayi <chinmayisuresh1998@gmail.com>
2022-01-12 21:50:59 +0530

    a

---
+++ statemgmt/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Parent</h1>
+     <child1/>
+    </div>
+  );
+}
+
+function child1(){
+  <h1>child1</h1>
+}
+
+export default App;
